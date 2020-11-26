5564ccbe2242831be01143569a3fc7509c4e1fb2
apsampaio <andre03.sampaio@gmail.com>
2020-11-26 19:04:06 -0300

    add src

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello from Theraphosidae</Text>
+    </View>
+  );
+};
+
+export default App;
