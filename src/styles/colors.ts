755210415d12831e974aa6c342e11643a4a0e146
apsampaio <andre03.sampaio@gmail.com>
2020-11-28 12:17:56 -0300

    add colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+    background: "#1D1D1F",
+    box: "#4D4B52",
+    boxForeground: "#313033",
+    blue: "#2CADF6",
+    oceanBlue: "#8BE9FD",
+    title: "#FFFFFF",
+    icon: "#9B98A6",
+    red: "#F9371C",
+    orange: "##F97C1C",
+    yellow: "#F9C81C",
+    green: "#41D0B6",
+    purple: "#6562FC",
+    pink: "#FF79C6"
+};
+
+export default colors;
