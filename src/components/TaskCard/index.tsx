cfd39e95854992dee4eab410f594b0b17cfd172c
apsampaio <andre03.sampaio@gmail.com>
2020-11-29 11:21:54 -0300

    add variable to task cards

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -5,13 +5,25 @@ import style from "./style";
 import { Text, View } from "react-native";
 import colors from "../../styles/colors";
 
-const TaskCard: React.FC = () => {
+export interface TaskData {
+  task: "Alimentar" | "Irrigar" | "Realojar";
+  quantity: number;
+  id: number;
+}
+
+const TaskCard: React.FC<TaskData> = ({ task, quantity }) => {
+  const TaskColor = {
+    Alimentar: colors.orange,
+    Irrigar: colors.blue,
+    Realojar: colors.red,
+  };
+
   return (
     <View style={style.container}>
-      <View style={[style.status, { backgroundColor: colors.orange }]} />
+      <View style={[style.status, { backgroundColor: TaskColor[task] }]} />
       <View style={{ marginLeft: 8 }}>
-        <Text style={style.task}>Alimentar</Text>
-        <Text style={style.quantity}>4 Tarântulas</Text>
+        <Text style={style.task}>{task}</Text>
+        <Text style={style.quantity}>{quantity} Tarântulas</Text>
       </View>
     </View>
   );
--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { View, FlatList, Text } from "react-native";
 
-import TaskCard from "../TaskCard";
+import TaskCard, { TaskData } from "../TaskCard";
 
 import style from "./style";
+
+const taskListData: TaskData[] = [
+  {
+    id: 0,
+    task: "Alimentar",
+    quantity: 3,
+  },
+  {
+    id: 1,
+    task: "Irrigar",
+    quantity: 5,
+  },
+  {
+    id: 2,
+    task: "Realojar",
+    quantity: 1,
+  },
+];
 
 const TaskList: React.FC = () => {
   return (
@@ -11,9 +29,11 @@ const TaskList: React.FC = () => {
       <Text style={style.title}>Hoje</Text>
       <FlatList
         horizontal
-        data={["1", "2", "3", "4", "5"]}
-        renderItem={({ item }) => <TaskCard key={item} />}
-        keyExtractor={(item) => item}
+        data={taskListData}
+        renderItem={({ item }) => (
+          <TaskCard task={item.task} quantity={item.quantity} id={item.id} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={style.taskList}
         ListFooterComponent={() => <View style={style.FooterItem} />}
         showsHorizontalScrollIndicator={false}
