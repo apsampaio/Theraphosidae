50a168372a4f2fda58d6b800340d1d7df8c95067
apsampaio <andre03.sampaio@gmail.com>
2021-01-02 17:01:53 -0300

    add icons

--- src/pages/Tarantula/index.tsx
+++ src/pages/Tarantula/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 import styles from "./styles";
 
 const Tarantula: React.FC = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.TabHeader}>
+        <TouchableOpacity>
+          <Icon
+            style={styles.TabHeaderButton}
+            name="arrow-left"
+            color="#FFFFFF"
+            size={24}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Icon
+            style={styles.TabHeaderButton}
+            name="menu"
+            color="#FFFFFF"
+            size={24}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default Tarantula;
--- src/pages/Tarantula/styles.ts
+++ src/pages/Tarantula/styles.ts
@@ -1,11 +1,26 @@
-import { StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+  },
 
-    container : {
+  TabHeader: {
+    flexDirection: "row",
+    height: 56,
+    backgroundColor: colors.boxForeground,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
 
-    }
-
+  TabHeaderButton: {
+    backgroundColor: colors.boxForeground,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default styles;
