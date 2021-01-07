40f216d56040058302da2c75a698e6e12af9f43d
apsampaio <andre03.sampaio@gmail.com>
2021-01-07 19:26:25 -0300

    add todo

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,12 +1,9 @@
-import { StyleSheet}  from 'react-native';
+import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create({
-    
-    container: {
-        backgroundColor: "#1D1D1F",
-        flex: 1,
-    }
-
+  container: {
+    backgroundColor: "#1D1D1F",
+  },
 });
 
 export default style;
