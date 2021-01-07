aa79ff2e22381d6442272cbbf4ad9042f44c8d08
apsampaio <andre03.sampaio@gmail.com>
2021-01-07 18:44:13 -0300

    add routes folder and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "react-native-gesture-handler";
 import React from "react";
+import { View, StatusBar } from "react-native";
 
-import Home from "./pages/Home";
-import Tarantula from "./pages/Tarantula";
-import History from "./pages/History";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./routes";
+
+import colors from "./styles/colors";
 
 const App: React.FC = () => {
-  return <History />;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor={colors.background} />
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
 };
 
 export default App;
