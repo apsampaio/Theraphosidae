5ad75e8f5f033f5de9b594fdc2e363d552574ef6
apsampaio <andre03.sampaio@gmail.com>
2021-01-15 20:21:13 -0300

    add shimmer component

--- src/components/Shimmer/ShimmerCalendar.tsx
+++ src/components/Shimmer/ShimmerCalendar.tsx
@@ -1,23 +1,53 @@
-// import React from "react";
-// import { StyleSheet, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Animated, View, Dimensions, Easing } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
-// import colors from "../../styles/colors";
+import colors from "../../styles/colors";
 
-// const style = StyleSheet.create({
-//   container: {
-//     height: 290,
-//     marginHorizontal: 16,
-//     borderRadius: 4,
-//   },
-// });
+const { width } = Dimensions.get("screen");
+const style = StyleSheet.create({
+  container: {
+    height: 290,
+    width,
+  },
+});
 
-// const ShimmerCalendar: React.FC = () => {
-//   return (
-//     <LinearGradient
-//       colors={[colors.box, colors.title]}
-//       style={style.container}
-//     />
-//   );
-// };
+const AnimatedLG = Animated.createAnimatedComponent(LinearGradient);
 
-// export default ShimmerCalendar;
+const ShimmerCalendar: React.FC = () => {
+  const animatedValue = new Animated.Value(0);
+
+  const easing = Easing.linear;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 1000,
+        easing,
+        useNativeDriver: true,
+      })
+    ).start();
+  });
+
+  const translateX = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-width * 2, width * 2],
+  });
+
+  return (
+    <View style={style.container}>
+      <AnimatedLG
+        colors={[colors.box, colors.icon, colors.icon, colors.box]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          transform: [{ translateX: translateX }],
+        }}
+      />
+    </View>
+  );
+};
+
+export default ShimmerCalendar;
