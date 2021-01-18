37cf491f440e366d504e457718e6c549dbdb6404
apsampaio <andre03.sampaio@gmail.com>
2021-01-18 18:33:08 -0300

    remove shimmer effects

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,21 +9,10 @@ import Separator from "../../components/Separator";
 import TaskList from "../../components/TaskList";
 import CardList from "../../components/CardList";
 
-import ShimmerCalendar from "../../components/Shimmer/ShimmerCalendar";
-
 const Home: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
   return (
     <ScrollView style={style.container} showsVerticalScrollIndicator={false}>
       <CalendarComponent />
-      <ShimmerCalendar />
       <Separator />
       <TaskList />
       <Separator />
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,8 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width } = Dimensions.get("screen");
 
 const style = StyleSheet.create({
   container: {
     backgroundColor: "#1D1D1F",
+  },
+
+  calendarShimmer: {
+    height: 370,
+    width: width - 32,
+    alignSelf: "center",
+    borderRadius: 4,
+    marginTop: 16,
   },
 });
 
