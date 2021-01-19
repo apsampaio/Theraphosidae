5e303fda9a501d45bdb6269c64ccd32a87b28f89
apsampaio <andre03.sampaio@gmail.com>
2021-01-19 18:45:42 -0300

    define stack and drawer

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -8,28 +8,16 @@ const App = createStackNavigator();
 
 import colors from "../styles/colors";
 
-import Home from "../pages/Home";
-import Tarantula from "../pages/Tarantula";
-import History from "../pages/History";
+import HomeScreenRoutes from "./homescreen.routes";
 
 const AppRoutes: React.FC = () => {
   return (
-    // <App.Navigator
-    //   screenOptions={{
-    //     headerShown: false,
-    //     cardStyle: { backgroundColor: colors.background },
-    //   }}
-    // >
-    //   <App.Screen name="Home" component={Home} />
-    //   <App.Screen name="Tarantula" component={Tarantula} />
-    //   <App.Screen name="History" component={History} />
-    // </App.Navigator>
     <Drawer.Navigator
       drawerStyle={{
         backgroundColor: colors.boxForeground,
       }}
     >
-      <Drawer.Screen name="Home" component={Home}></Drawer.Screen>
+      <Drawer.Screen name="Início" component={HomeScreenRoutes} />
     </Drawer.Navigator>
   );
 };
