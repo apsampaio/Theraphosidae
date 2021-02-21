bf226464d6e3c733ec58a910c40c152b3487c026
apsampaio <andre03.sampaio@gmail.com>
2021-02-21 17:06:06 -0300

    add remove and save providers

--- src/data/database/provider/AsyncStorageProvider.ts
+++ src/data/database/provider/AsyncStorageProvider.ts
@@ -1,17 +1,41 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-interface save {
+interface saveDTO {
   key: string;
-  data: object[];
+  data: parsedData;
+}
+
+interface removeDTO {
+  key: string;
+  id: string;
+}
+
+interface parsedData {
+  id: string;
 }
 
 export default class AsyncStorageProvider {
-  public async save({ key, data }: save) {
+  public async save({ key, data }: saveDTO) {
     try {
       const storaged = await AsyncStorage.getItem(key);
       if (!storaged) return;
       const storagedData = JSON.parse(storaged);
-      const newData = [...storagedData, ...data];
+      const filteredData = storagedData.filter(
+        ({ id }: parsedData) => id !== data.id
+      );
+      const newData = [...filteredData, data];
+      await AsyncStorage.setItem(key, JSON.stringify(newData));
+    } catch (e) {
+      // SAVING ERROR
+    }
+  }
+
+  public async remove({ key, id }: removeDTO) {
+    try {
+      const storaged = await AsyncStorage.getItem(key);
+      if (!storaged) return;
+      const storagedData = JSON.parse(storaged);
+      const newData = storagedData.filter((data: parsedData) => data.id !== id);
       await AsyncStorage.setItem(key, JSON.stringify(newData));
     } catch (e) {
       // SAVING ERROR
