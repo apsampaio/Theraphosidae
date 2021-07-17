763e6e009d2e7a7de36aff575674d3efa15ed80e
apsampaio <andre03.sampaio@gmail.com>
2021-07-17 20:45:10 -0300

    add tab bar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
 
-export default function App() {
+import React, { useEffect } from "react";
+import { View, StatusBar } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { Routes } from "./src/routes";
+
+import colors from "./src/styles/colors";
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,69 @@
-import React from "react";
-import AppRoutes from "./drawer.routes";
-import { DrawerProvider } from "../hooks/Drawer";
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Feather } from "@expo/vector-icons";
+
+import colors from "../styles/colors";
+
+function BlogScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Blog!</Text>
+    </View>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const Routes: React.FC = () => {
   return (
-    <DrawerProvider>
-      <AppRoutes />
-    </DrawerProvider>
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: colors.title,
+        inactiveTintColor: colors.icon,
+        showLabel: false,
+        keyboardHidesTabBar: true,
+        style: {
+          backgroundColor: colors.background,
+          borderTopColor: colors.purple,
+          borderTopWidth: StyleSheet.hairlineWidth,
+        },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Blog") {
+            return <Feather name={"file-text"} size={size} color={color} />;
+          } else if (route.name === "Home") {
+            return <Feather name={"calendar"} size={size} color={color} />;
+          } else {
+            return <Feather name={"menu"} size={size} color={color} />;
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Blog" component={BlogScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 };
 
-export default Routes;
+export { Routes };
